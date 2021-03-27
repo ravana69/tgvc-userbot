e492b08550779e506e119114e12c6b7055c4beef
Ravana <47528708+ravana69@users.noreply.github.com>
2021-03-28 00:13:50 +0530

    Update radio.py

--- plugins/vc/radio.py
+++ plugins/vc/radio.py
@@ -22,6 +22,10 @@ To stop use !stop command
 
 
 # Commands available only for anonymous admins
+async def anon_filter(_, __, m: Message):
+    return bool(m.from_user is None and m.sender_chat)
+
+anonymous = filters.create(anon_filter)
 
 GROUP_CALLS = {}
 FFMPEG_PROCESSES = {}
